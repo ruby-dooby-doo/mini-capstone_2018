13f4f8f3f1e2f34f0779f5eb9c4f2ab15dce52bd
JackMarx <seriousfools@gmail.com>
2018-05-16 14:54:11 -0500

    split

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,8 @@
-# config/routes.rb
 Rails.application.routes.draw do
   namespace :api do
     get '/products' => 'products#index'
     get '/products/:id' => 'products#show'
     post '/products' => 'products#create'
-    patch '/products/:id' => 'products#update'
-    delete '/products/:id' => 'products#destroy'
-  end
-
-  namespace :client do
-    get '/products' => 'products#index'
-    get '/products/new' => 'products#new'
-    post '/products' => 'products#create'
-    get '/products/:id' => 'products#show'
-    get '/products/:id/edit' => 'products#edit'
     patch '/products/:id' => 'products#update'
     delete '/products/:id' => 'products#destroy'
   end
