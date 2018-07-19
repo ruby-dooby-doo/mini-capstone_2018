d931f429cef705ec447e4ea610699dbdc6f36de4
Brian <riceb53@gmail.com>
2018-07-19 18:57:50 -0500

    add create route, add id to show.json.jbuilder

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
 
     get '/orders/:id' => 'orders#show'
     post '/orders' => 'orders#create'
+
+    post '/carted_products' => 'carted_products#create'
   end
 end
